d889af50ed31dd96c49fd2ef72f9a84487c66c0b
Jeannie Branstrator <jmbranstrator@gmail.com>
2022-06-18 18:15:30 -0500

    added secondary order bby for sql

--- src/components/Feeling/Feeling.jsx
+++ src/components/Feeling/Feeling.jsx
@@ -2,12 +2,6 @@ import { useHistory } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { useState } from 'react';
 
-import Radio from '@mui/material/Radio';
-import RadioGroup from '@mui/material/RadioGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormControl from '@mui/material/FormControl';
-import FormLabel from '@mui/material/FormLabel';
-
 function Feeling(){
     const dispatch = useDispatch();
     const history = useHistory();
